f01d2ae8e93653bb2c05b941c35c789e8fa5f2f9
Eranmonnie <ajalaferanmi123@gmail.com>
2023-12-19 23:51:11 +0100

    resizing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between md:justify-normal gap-14">
-      <Link to="/" className="text-lg font-inter-semibold lg:text-3xl">
+      <Link to="/" className="text-lg font-inter-semibold lg:text-2xl">
         Feranmi&reg;
       </Link>
       <div>
-        <Link to="/" className="text-lg font-inter-semibold lg:text-3xl">
+        <Link to="/" className="text-lg font-inter-semibold lg:text-2xl">
           Projects
         </Link>
-        <Link to="/" className="text-lg ml-2 font-inter-semibold lg:text-3xl">
+        <Link to="/" className="text-lg ml-2 font-inter-semibold lg:text-2xl">
           About
         </Link>
       </div>
       <Link
         to="/"
-        className="text-lg font-inter-semibold hidden md:inline-flex ml-auto lg:text-3xl"
+        className="text-lg font-inter-semibold hidden md:inline-flex ml-auto lg:text-2xl"
       >
         ajalaferanmi123@gmail.com
       </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ function Home() {
           (2020-2023)
         </h1>
       </div>
-      <p className="mt-5 text-xl text-left lg:text-5xl lg:w-[50%] lg:mt-10 "> 
+      <p className="mt-5 text-xl text-left lg:text-4xl lg:w-[50%] lg:mt-10 "> 
       Have a look at some of the projects I’ve worked on. Clients vary from lifestyle to tech and design companies.
       </p>
       <Works name ={`Nerflix`} year={`2023`} />
@@ -22,10 +22,10 @@ function Home() {
 
       <div className="flex flex-col gap-2 pt-10 lg:flex-row lg:gap-[20%] lg:mt-64">
       <h2 className="text-xl text-left lg:text-2xl">(002)</h2>
-        <div className="lg:w-[45%]">
+        <div className="lg:w-[55%]">
         <h2 className="text-xl text-left lg:text-4xl">Looking to grow your business through the power of great digital design?</h2>
         <h2 className="text-xl mt-5 text-left lg:text-4xl">Reach out to get the conversation started about your digital design needs.</h2>
-        <button className="py-5 mt-16 px-7 border rounded-full w-[300px]">ajalaferanmi123@gmail.com</button>
+        <button className="py-5 mt-16 px-7 border rounded-full w-[300px] lg:w-[320px] lg:text-xl">ajalaferanmi123@gmail.com</button>
         </div>
         
       </div>
