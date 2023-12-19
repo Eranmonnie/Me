05046bd8d1cd86da0d75aad50ceb52634420aa77
Eranmonnie <ajalaferanmi123@gmail.com>
2023-12-19 16:15:09 +0100

    init

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,23 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between p-3 md:justify-normal gap-14">
-      <Link to="/" className="text-lg font-inter-semibold">Feranmi&reg;</Link>
+      <Link to="/" className="text-lg font-inter-semibold">
+        Feranmi&reg;
+      </Link>
       <div>
-        <Link to="/" className="text-lg font-inter-semibold" >Projects</Link>
-        <Link to="/" className="text-lg ml-2 font-inter-semibold">About</Link>
+        <Link to="/" className="text-lg font-inter-semibold">
+          Projects
+        </Link>
+        <Link to="/" className="text-lg ml-2 font-inter-semibold">
+          About
+        </Link>
       </div>
-      <Link to="/" className="text-lg font-inter-semibold hidden md:inline-flex ml-auto">ajalaferanmi123@gmail.com</Link>
+      <Link
+        to="/"
+        className="text-lg font-inter-semibold hidden md:inline-flex ml-auto"
+      >
+        ajalaferanmi123@gmail.com
+      </Link>
     </div>
   );
 };
