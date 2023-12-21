31e75f9768a625f3aa4677c69f46dceed4c2e95d
Eranmonnie <ajalaferanmi123@gmail.com>
2023-12-21 20:41:48 +0100

    idek section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import Profile from '../components/Profile'
 import Question from '../components/Question'
 import Solution from '../components/Solution'
+import Footer from '../components/Footer'
 
 const About = () => {
   return (
@@ -24,6 +25,7 @@ const About = () => {
 
      <Solution heading={`Defining the business goal and `} data={`Everything starts by getting clarity around what we are trying to achieve. What should the website’s main goal be? What does project succes mean for your business? Where will the business be in 3 years? These are just some questions which we’ll work together on to define the best path forward when it comes to your digital needs.`} />
      <Solution  heading={`Strategic visual design and art direction.`} data={`The visual language of the website will define how the visitor connects with the product or service. Knowing who the target audience is and what everybody else is doing in the field is crucial to get the best result from the website we’ll create together.`}/>
+     <Footer />
     </>
   )
 }
