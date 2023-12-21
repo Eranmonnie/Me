bec7ec50591e9c53c99f7076ac4ebbee75ca6365
Eranmonnie <ajalaferanmi123@gmail.com>
2023-12-21 20:18:17 +0100

    idek section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const profile = () => {
   return (
     <>
-      <div className="w-[99.7%] mt-44  mb-24 gap-5 lg:mb-44">
+      <div className="w-[99.7%] mt-28  mb-24 gap-5 lg:mb-44">
         <div className="">
           <img
             src="src\images\Screenshot (26).png"
--- src/components/Solution.tsx
+++ src/components/Solution.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Solution = ({heading,data}) => {
+const Solution = ({ heading, data }) => {
   return (
-   <>
-   <div className="flex flex-col gap-8 mb-20">
-        <h3 className="text-lg">
-            {heading}
-        </h3>
+    <>
+      <div className="lg:w-[75%] lg:ml-auto">
+        <div className="lg:flex lg:flex-row lg:gap-[70px]">
+          <h3 className="hidden lg:inline ">01.</h3>
 
-        <p className="text-lg">
-            {data}
-        </p>
-   </div>
-   </>
-  )
-}
+          <div className="lg:w-[60%]">
+            <h3 className="text-lg mb-7">{heading}</h3>
 
-export default Solution
+            <p className="text-lg">{data}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Solution;
