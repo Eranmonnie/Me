f8d792a98edc679f4800961bbd511a6a265e8b1b
Eranmonnie <ajalaferanmi123@gmail.com>
2024-01-02 14:49:42 +0100

    made child elements animate at the same time when parent div is hovered over

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
-import { motion, inView, animate } from "framer-motion";
-
+import { useAnimation, motion } from "framer-motion";
 
 interface worksProps {
   name: string;
@@ -9,27 +8,85 @@ interface worksProps {
 }
 
 const Works: FC<worksProps> = ({ name, year, data }) => {
+  const controls = useAnimation();
+
+  const worksvariants = {
+    initial: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  const imagevariance = {
+    initial: {
+      scale: 1,
+    },
+    animate: {
+      scale: 1.03,
+
+      transition: {
+        duration: 0.3,
+        delay: 0.1,
+      },
+    },
+  };
+
+  const popupvariance = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 0.6,
+
+      transition: {
+        duration: 0.3,
+        delay: 0.1,
+      },
+    },
+  };
+
+  const handleHover = () => {
+    controls.start("animate");
+  };
+
+  const handleHoverEnd = () => {
+    controls.start("initial");
+  };
+
   return (
     <>
       <div className="w-[99.7%] flex flex-col mb-36 gap-5 lg:mb-44 ">
-        <motion.div className="w-full rounded-xl overflow-hidden relative"
-        initial={{ opacity:0 }}
-        whileInView={{opacity:1}}
-        transition={{duration:1}}
+        <motion.div
+          className="w-full rounded-xl overflow-hidden relative"
+          variants={worksvariants}
+          initial="initial"
+          whileInView="visible"
+          onHoverStart={handleHover}
+          onHoverEnd={handleHoverEnd}
+          whileHover={{ scale: 1 }}
         >
           <motion.img
-            whileHover={{ scale: 1.03 }}
-            transition={{ duration: 0.3, delay: 0.1 }}
+            variants={imagevariance}
+            initial="initial"
+            animate={controls}
             src="https://res.cloudinary.com/djtlzuxfa/image/upload/v1703443457/Screenshot_147_ghd6mh.png"
             className="object-cover w-full "
             alt="///"
           />
-          <div className="hidden lg:absolute lg:top-[50%] lg:left-[50%] lg:translate-y-[-50%] lg:translate-x-[-50%] lg:w-44 lg:h-44 lg:bg-white lg:rounded-full lg:p-14 lg:flex lg:justify-center lg:items-center">
-              <h2 className="text-center text-lg">
-                view case
-              </h2>
-          </div>
-          
+          <motion.div
+            variants={popupvariance}
+            initial="initial"
+            animate={controls}
+            className="hidden lg:absolute lg:top-[50%] lg:left-[50%] lg:translate-y-[-50%] lg:translate-x-[-50%] lg:w-44 lg:h-44 lg:bg-white lg:rounded-full lg:p-14 lg:flex lg:justify-center lg:items-center"
+          >
+            <h2 className="text-center text-lg">view case</h2>
+          </motion.div>
         </motion.div>
 
         <div className="flex justify-between lg:gap-[21%]">
