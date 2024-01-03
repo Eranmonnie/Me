509585a08e7d001e05056e56dc6269781e1fbf49
Eranmonnie <ajalaferanmi123@gmail.com>
2024-01-03 18:44:29 +0100

    added project details

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -10,28 +10,28 @@ const Projects = () => {
       <Banner first={`selected works`} second={`(2020-2023)`} align={true} />
       <div className="mb-48">
         <Hero
-          message={`Have a look at some of the projects I’ve worked on. They vary from lifstand alone front-end applications to full fledged fulstack ones.`}
+          message={`Have a look at some of the projects I’ve worked on. They vary from presonal to academic front-end applications and full fledged fulstack ones.`}
         />
       </div>
       <Works
         name={`Nerflix`}
         year={`2023`}
-        data={`Ecommerce website for the first smart mailbox Custo®.`}
+        data={`Netflix clone created with react and tailwind css.`}
       />
       <Works
         name={`Laracast`}
         year={`2023`}
-        data={`Ecommerce website for the first smart mailbox Custo®.`}
+        data={`Fullstack blogging website created with Laravel and tailwind css.`}
       />
       <Works
         name={`Worddle clone`}
         year={`2023`}
-        data={`Ecommerce website for the first smart mailbox Custo®.`}
+        data={`wordle game clone created with HTML, CSS, and Javascript.`}
       />
       <Works
-        name={`custo`}
-        year={`2020`}
-        data={`Ecommerce website for the first smart mailbox Custo®.`}
+        name={`Ray Valley`}
+        year={`2023`}
+        data={`Simple farm website for my opensource project powered by Couchcms and php.`}
       />
       <Question
         one={`Looking to grow your business through the power of great digital design?`}
